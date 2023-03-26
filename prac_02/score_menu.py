d24d295074983ed1ad54e1bb7db91edd47666f5d
lmtk2 <lmtk1234@gmail.com>
2023-03-26 23:53:48 +0800

    add score_menu_prac_2

--- prac_02/score_menu.py
+++ prac_02/score_menu.py
@@ -4,23 +4,48 @@ MENU = """(G)et a valid score (must be 0-100 inclusive)
           (Q)uit"""
 
 def main():
+    score = input("your score : ")
     print(MENU)
     choice = input(">>> ").upper()
     while choice != "Q":
         if choice == "G":
-            celsius = float(input("Celsius>> "))
-            fahrenheit = change_celsius_to_fahrenheit(celsius)
-            print(f"Result: {fahrenheit:.2f} F")
-        elif choice == "F":
-            fahrenheit = float(input("fahrenheit>> "))
-            celsius = change_fahrenheit_to_celsius(fahrenheit)
-            print(f"Result: {celsius:.2f} C")
-def change_celsius_to_fahrenheit(celsius):
-    return celsius * 9.0 / 5 + 32
+            result = get_valid_score
+            print(result)
+        elif choice == "P":
+            celsius = print_result(score)
+            print(result)
+        elif choice == "S":
+            result = show_star(score)
+            print(result)
+        else:
+            print("Invalid option")
+        print(MENU)
+        choice = input(">>> ").upper()
+    print("Thank you.")
 
-def change_fahrenheit_to_celsius(fahrenheit):
-    return  5/9 * (fahrenheit - 32)
+def get_valid_score():
+    while True:
+        try:
+            score = int(input("Enter a score (0-100 inclusive): "))
+            if score < 0 or score > 100:
+                raise ValueError
+            return score
+        except ValueError:
+            print("Invalid score. Please try again.")
 
+def print_result(score):
+    while score != 0:
+        if score > 100:
+            print("Invalid score")
+        elif 90 > score > 50:
+            print("Passable")
+        elif 100 >= score >= 90:
+            print("Excellent")
+        else:
+            print("Bad")
+def show_star(score):
+    result = "*" * score
+    print(result)
 main()
 
 
