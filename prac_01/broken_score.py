f40d7d2e2abcff94df0b64bef0fcaac47dc55795
lmtk2 <lmtk1234@gmail.com>
2023-03-26 23:18:24 +0800

    add score_prac_2

--- prac_01/broken_score.py
+++ prac_01/broken_score.py
@@ -5,17 +5,19 @@ Broken program to determine score status
 
 # TODO: Fix this!
 
-score = float(input("Enter score: "))
 
-
-while score != 0:
-    if score > 100:
-        print("Invalid score")
-    elif 90 > score > 50:
-        print("Passable")
-    elif 100 >= score >= 90:
-        print("Excellent")
-    else:
-        print("Bad")
+def main():
     score = float(input("Enter score: "))
-print("Sorry")
+    print(score_review(score))
+
+def score_review(score):
+    if score > 100:
+        return "Invalid score"
+    elif 90 > score > 50:
+        return "Passable"
+    elif 100 >= score >= 90:
+        return "Excellent"
+    else:
+        return "Bad"
+
+main()
