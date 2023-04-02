1f48eb822bedd4cd4dcf76bf16a8c0217dc58d93
lmtk2 <lmtk1234@gmail.com>
2023-04-02 23:46:40 +0800

    added to prac 3

---
+++ prac_03/random.py
@@ -0,0 +1,19 @@
+# What did you see on line 1?
+# 9,17,5,14,20,10,19
+# What was the smallest number you could have seen, what was the largest?
+# 5 is smallest and 20 is largest
+# What did you see on line 2?
+#3/7//9/5/3
+# What was the smallest number you could have seen, what was the largest?
+# 3 and 9
+# Could line 2 have produced a 4?
+# no
+# What did you see on line 3?
+#2.586342439727643 / 2.542876574520683/ 4.697228567615916/3.6468393819307727/4.709858807999106
+# What was the smallest number you could have seen, what was the largest?
+# 2.586342439727643 and 4.709858807999106
+import random
+
+random_number = random.randint(1, 100)
+
+print(random_number)
